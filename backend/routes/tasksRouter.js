42121601d26898201f7c9d09195eaca78690c05a
PabloDev <yauripablo70@gmail.com>
2025-07-13 19:31:09 -0500

    Logica para agregar una nueva tarea

--- backend/routes/tasksRouter.js
+++ backend/routes/tasksRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getTasks } = require('../controllers/tasksController');
+const { getTasks, createTask } = require('../controllers/tasksController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 //Ruta con autenticación
 router.get('/tareas',authMiddleware, getTasks);
+router.post('/nueva-tarea', authMiddleware, createTask);
 
 module.exports = router;
